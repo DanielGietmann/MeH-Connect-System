e3f15b4596f4e9b0b3af5dd9bbbea6e525c4115d
Daniel Gietmann <danielgietmann@icloud.com>
2021-05-10 21:15:53 +0200

    Add files via upload

---
+++ Runtime/MeHMySqlConnector.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace mehmysql
+{
+    public class MeHMySqlConnector : MonoBehaviour
+    {
+
+        static string connString;
+
+
+
+        public static void CreateDBConString(string Server, string User, string Database, string Port,string Password)
+        {
+            connString = "server="+ Server +";user="+ User +";database="+Database+";port="+Port+";password="+Password+"";
+        }
+                    
+
+        public static void DBConnection(string sqlbefehl)
+        {
+            string connStr = connString;
+            MySqlConnection conn = new MySqlConnection(connStr);
+            try
+            {
+                Debug.Log("Connecting to MySQL...");
+                conn.Open();
+
+                string sql = sqlbefehl;
+                MySqlCommand cmd = new MySqlCommand(sql, conn);
+                object result = cmd.ExecuteScalar();
+                if (result != null)
+                {
+                    int r = Convert.ToInt32(result);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.ToString());
+            }
+        }
+    }
+}
---
+++ Runtime/NetWorkManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace mehmysql
+{
+    public class NetWorkManager : MonoBehaviour
+    {
+
+        public string Server;
+        public string User;
+        public string Database;
+        public string Port;
+        public string Password;
+
+
+        private void Awake()
+        {
+            MeHMySqlConnector.CreateDBConString(Server, User, Database, Port, Password);
+        }
+
+    }
+}
